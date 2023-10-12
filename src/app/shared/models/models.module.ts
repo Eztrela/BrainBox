1d629a1c44831f7f05d1c82272be846d96a80458
Lucas Nobrega <lucaspdn04@gmail.com>
2023-10-11 23:56:46 -0300

    add barrel file, user class, models module

---
+++ src/app/shared/models/models.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { User } from "../models";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    User
+  ]
+})
+export class ModelsModule { }
