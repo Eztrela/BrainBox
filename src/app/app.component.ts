e4dc6c8f49abaafc3a347599a36ca9df853d109e
Lucas Nobrega <105940701+lucas-pdnobrega@users.noreply.github.com>
2023-10-17 11:39:32 -0300

    task CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MemoryBox, Note, Tag} from "./shared/models";
 import {Task} from "./shared/models";
+import {TaskService} from "./shared/services/task.service";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ import {Task} from "./shared/models";
 export class AppComponent {
   title = 'brainbox'
   memoryboxes: Array<MemoryBox> = new Array<MemoryBox>();
-  constructor() {
+  constructor(private taskService: TaskService) {
     let memorybox = new MemoryBox(1, "title")
 
     this.memoryboxes.push(memorybox);
@@ -21,8 +22,6 @@ export class AppComponent {
     memorybox.inserirTask(task1);
     memorybox.inserirTask(task2);
 
-    console.log(memorybox.localizarTask(1));
-    console.log(memorybox.localizarTask(123451));
     console.log(this.memoryboxes[0].removerTask(2));
 
     let note1 = new Note(1, "content1");
@@ -31,8 +30,6 @@ export class AppComponent {
     memorybox.inserirNote(note1);
     memorybox.inserirNote(note2);
 
-    console.log(memorybox.localizarNote(1));
-    console.log(memorybox.localizarNote(123451));
     console.log(this.memoryboxes[0].removerNote(2));
 
     let tag1 = new Tag(1, "title1", "#00000");
@@ -45,11 +42,17 @@ export class AppComponent {
 
     console.log(memorybox)
 
-    console.log(memorybox.localizarTag(1));
-    console.log(memorybox.localizarTag(123451));
     console.log(this.memoryboxes[0].removerTag(2));
 
     console.log(memorybox)
+
+    /*TESTES DE TASKSERVICE*/
+    this.taskService.inserir(task1);
+    this.taskService.inserir(task2);
+
+    console.log(this.taskService.listar());
+    console.log(`${this.taskService.get(1)} \n ${this.taskService.get(2)}`)
+    console.log(this.taskService.remover(2));
 
 
   }
---
+++ src/app/shared/services/task.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../models';
+import { TASKS } from "../models/TASKS";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private _tasks: Array<Task>;
+
+  constructor() {
+    this._tasks = TASkS
+  }
+
+  listar():Array<Task> {
+    return this._tasks;
+  }
+
+  inserir(task: Task) {
+    let idx = this.localizar(task.id);
+    if (idx < 0) {
+      this._tasks.push(task);
+    } else {
+      this._tasks[idx] = task;
+    }
+  }
+
+  /*const handleUpdate = (fieldName: string, fieldValue: string | number) => {
+    if (fieldName in myObject) {
+        myObject = {...myObject, fieldName: fieldValue};
+    }
+}*/
+
+  editar(id: number) {
+    let idx: number = this.localizar(id);
+    if (idx >= 0) {
+      this._tasks[idx] = false;
+    } else {
+      return false
+    }
+  }
+
+  remover(id: number) {
+    let idx = this.localizar(id);
+    if (idx >= 0) {
+      return this._tasks.splice(idx, 1)[0];
+    } else {
+      return false;
+    }
+  }
+
+  get(id: number): Task | false {
+    let idx: number = this.localizar(id);
+    if (idx >= 0) {
+      return this._tasks[idx];
+    } else {
+      return false;
+    }
+  }
+
+  localizar(id: number): number {
+    return this._tasks.findIndex((t:Task):boolean => (t.id === id));
+  }
+
+}
