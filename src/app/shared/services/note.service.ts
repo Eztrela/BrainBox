29dc8d275ac9aa35537724888e471d40def4f3ac
eztrela <pablolm.estrela@gmail.com>
2023-10-17 20:14:35 -0300

    adicionando geração de id incremental e correção no retorno dos metodos e tratamento de erro

--- src/app/shared/services/note.service.ts
+++ src/app/shared/services/note.service.ts
@@ -26,38 +26,43 @@ export class NoteService {
     }
   }
 
-  editar(id: number, fieldName: string, fieldValue: number | string | Date) {
+  editar(id: number, fieldName: string, fieldValue: number | string ) {
     let idx: number = this.localizar(id);
-    if (idx < 0) return false;
+    if (idx < 0) 
+      throw new Error(`A anotação de id ${id} não foi encontrada`)
 
     let note = this._notes[idx];
 
-    if (!(fieldName in note)) return false;
+    if (!(fieldName in note))
+     throw new Error(`A anotação não possui ${fieldName}`)
 
+    if(typeof((note as any)[fieldName]) !== typeof(fieldValue))
+      throw new Error(`Valor a ser atualizado é inválido! ${fieldValue}`);
     (note as any)[fieldName] = fieldValue;
+    
     return true;
   }
 
   remover(id: number) {
     let idx = this.localizar(id);
-    if (idx >= 0) {
-      return this._notes.splice(idx, 1)[0];
-    } else {
-      return false;
-    }
+    if (idx < 0) 
+      throw new Error(`A anotação de id ${id} não foi encontrada`)
+    return this._notes.splice(idx, 1)[0];
   }
 
-  get(id: number): Note | false {
+  get(id: number): Note{
     let idx: number = this.localizar(id);
-    if (idx >= 0) {
-      return this._notes[idx];
-    } else {
-      return false;
-    }
+    if (idx < 0) 
+      throw new Error(`A anotação de id ${id} não foi encontrada`)
+    return this._notes[idx];
   }
 
   localizar(id: number): number {
     return this._notes.findIndex((t:Note):boolean => (t.id === id));
+  }
+
+  generateID(): number{
+    return (this._notes.length > 0) ? this._notes[ this._notes.length -1].id + 1 : 1;
   }
 
 }
