88036e5a16ff5b7b6585427aa3bcfd27956918d2
Lucas Nobrega <105940701+lucas-pdnobrega@users.noreply.github.com>
2023-10-27 11:34:58 -0300

    initial tests

--- src/app/shared/services/note.service.ts
+++ src/app/shared/services/note.service.ts
@@ -7,26 +7,26 @@ import { NOTES } from "../models/NOTES";
 })
 export class NoteService {
 
-  private _notes: Array<Note>;
+  private _url: string = 'http://localhost:3000/notes';
 
   constructor() {
-    this._notes = NOTES
   }
 
-  listar():Array<Note> {
-    return this._notes;
+  async listar():Promise<Note[]> {
+    const data = await fetch(this._url);
+    return await data.json() ?? [];
   }
 
   inserir(note: Note) {
     let idx = this.localizar(note.id);
     if (idx >= 0) throw new Error(`Anotação de id ${note.id} já existe`);
     this._notes.push(note);
-    
+
   }
 
   editar(id: number, fieldName: string, fieldValue: number | string ) {
     let idx: number = this.localizar(id);
-    if (idx < 0) 
+    if (idx < 0)
       throw new Error(`A anotação de id ${id} não foi encontrada`)
 
     let note = this._notes[idx];
@@ -37,22 +37,22 @@ export class NoteService {
     if(typeof((note as any)[fieldName]) !== typeof(fieldValue))
       throw new Error(`Valor a ser atualizado é inválido! ${fieldValue}`);
     (note as any)[fieldName] = fieldValue;
-    
+
     return true;
   }
 
   remover(id: number) {
     let idx = this.localizar(id);
-    if (idx < 0) 
+    if (idx < 0)
       throw new Error(`A anotação de id ${id} não foi encontrada`)
     return this._notes.splice(idx, 1)[0];
   }
 
-  get(id: number): Note{
-    let idx: number = this.localizar(id);
-    if (idx < 0) 
+  async get(id: number): Promise<Note>{
+    const data = await fetch(`${this.url}/${id}`)
+    if (data === undefined)
       throw new Error(`A anotação de id ${id} não foi encontrada`)
-    return this._notes[idx];
+    return await data.json();
   }
 
   localizar(id: number): number {
