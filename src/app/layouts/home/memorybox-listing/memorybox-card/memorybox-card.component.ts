349dfab9964a1ef5fcd55ee1131bd1848f2f5755
Lucas Nobrega <lucaspdn04@gmail.com>
2023-11-07 20:05:57 -0300

    implement memorybox card getters

--- src/app/layouts/home/memorybox-listing/memorybox-card/memorybox-card.component.ts
+++ src/app/layouts/home/memorybox-listing/memorybox-card/memorybox-card.component.ts
@@ -3,6 +3,7 @@ import {MemoryBox, Note, Tag, Task} from "../../../../shared/models";
 import {TagService} from "../../../../shared/services/tag.service";
 import {TaskService} from "../../../../shared/services/task.service";
 import {NoteService} from "../../../../shared/services/note.service";
+import {forkJoin} from "rxjs";
 
 @Component({
   selector: 'app-memorybox-card',
@@ -19,21 +20,21 @@ export class MemoryboxCardComponent implements OnInit {
   constructor(private taskService:TaskService, private tagService: TagService, private noteService: NoteService) {
   }
   ngOnInit() {
-    for (let id in this.memorybox.tasks) {
-      this.taskService.getById(Number(id)).subscribe((task:Task) => {
-        this.tasks.push(task);
-      })
-    }
-    for (let id in this.memorybox.tags) {
-      this.tagService.getById(Number(id)).subscribe((tag:Tag) => {
-        this.tags.push(tag);
-      })
-    }
-    for (let id in this.memorybox.notes) {
-      this.noteService.getById(Number(id)).subscribe((note:Note) => {
-        this.notes.push(note);
-      })
-    }
+    const taskRequests = this.memorybox.tasks.map(id => this.taskService.getById(Number(id)));
+    const tagRequests = this.memorybox.tags.map(id => this.tagService.getById(Number(id)));
+    const noteRequests = this.memorybox.notes.map(id => this.noteService.getById(Number(id)));
+
+    forkJoin([...taskRequests]).subscribe((taskResults: Task[]) => {
+      this.tasks = taskResults;
+    });
+
+    forkJoin([...tagRequests]).subscribe((tagResults: Tag[]) => {
+      this.tags = tagResults;
+    });
+
+    forkJoin([...noteRequests]).subscribe((noteResults: Note[]) => {
+      this.notes = noteResults;
+    });
   }
 
 
