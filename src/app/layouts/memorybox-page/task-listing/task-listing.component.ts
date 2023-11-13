4801eb3a9f381a3b905fb37b70f65ef3c800c1a8
Eztrela <pablolm.estrela@gmail.com>
2023-11-13 00:11:39 -0300

    inicio remoção de tasks e tentando corrigir listagem de tags nas tasks

--- src/app/layouts/memorybox-page/task-listing/task-listing.component.ts
+++ src/app/layouts/memorybox-page/task-listing/task-listing.component.ts
@@ -4,6 +4,7 @@ import { MatTableDataSource, MatTableModule} from '@angular/material/table'
 import { forkJoin, map } from "rxjs";
 import { ITask } from 'src/app/shared/interfaces/itask';
 import { TagService } from 'src/app/shared/services/tag.service';
+import { MemoryboxService } from 'src/app/shared/services/memorybox.service';
 
 @Component({
   selector: 'app-task-listing',
@@ -14,7 +15,7 @@ export class TaskListingComponent implements OnInit{
   @Input() memorybox: MemoryBox = new MemoryBox(0,"",0);
   public datasource: MatTableDataSource<ITask> = new MatTableDataSource<ITask>();
   public tags: Array<Tag> = new Array<Tag>();
-  constructor(private tagService:TagService){
+  constructor(private tagService:TagService, private memoryBoxService: MemoryboxService){
   }
   
   ngOnInit(): void {
@@ -24,5 +25,15 @@ export class TaskListingComponent implements OnInit{
       this.tags.push(...tags);
     });
     this.datasource = new MatTableDataSource<ITask>(this.memorybox.tasks);
+  }
+
+  deleteTask(task: ITask){
+    const removedTask = this.memorybox.removerTask(task.id)
+    if(removedTask){
+      this.memoryBoxService.update(this.memorybox.id, this.memorybox).subscribe(memoryBoxAtualizado =>{
+        this.memorybox = memoryBoxAtualizado;
+        this.datasource.data = [...this.memorybox.tasks]
+      })
+    }
   }
 }
