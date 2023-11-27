6b43fffd1b91e1fb384a7bb86ad946e5712b3d10
Lucas Nobrega <lucaspdn04@gmail.com>
2023-11-27 20:22:17 -0300

    remove unused code in sidenav

--- src/app/layouts/components/sidenav/sidenav.component.ts
+++ src/app/layouts/components/sidenav/sidenav.component.ts
@@ -15,57 +15,6 @@ import {ITag} from "../../../shared/interfaces/itag";
 })
 export class SidenavComponent implements OnInit {
   @Input() memoryBoxes:Array<MemoryBox> = new Array<MemoryBox>();
-  tags!: Array<Tag>;
-  isMemoryBoxesCollapsed: boolean = true;
-  isTagsCollapsed: boolean = true;
-  @Input() page!: string;
-  constructor(private dialog:MatDialog, private tagService: TagService) {
-  }
-  ngOnInit() {
-    this.tagService.getAll().subscribe((res) => {
-      this.tags = res;
-    })
-  }
-
-  toggleMemoryBoxes() {
-    this.isMemoryBoxesCollapsed = !this.isMemoryBoxesCollapsed;
-  }
-
-  toggleTags() {
-    this.isTagsCollapsed = !this.isTagsCollapsed;
-  }
-
-  deleteTag(tagARemover: number | undefined) {
-    if (tagARemover) {
-    const idx = this.tags.findIndex((tag)=> {
-      return tag.id === tagARemover;
-    })
-    this.tagService.delete(tagARemover).subscribe(
-      (obj) => {
-        console.log(obj);
-        this.tags.splice(idx, 1);
-      })
-    }
-  }
-
-  editTag() {
-  }
-
-  openAddTagDialog() {
-    const dialogRef = this.dialog.open(CreateTagDialogComponent, {
-      data: {},
-      panelClass: 'tag-dialog-container'
-    });
-
-    dialogRef.afterClosed().subscribe((res) => {
-      if (res) {
-        let idx = this.tags.length > 0 ? Math.max(...this.tags.map(tag => {
-          return tag.id ? tag.id : 0
-        })) + 1 : 1;
-        let tag = new Tag(idx, {title: res.title, color: res.color});
-        this.tagService.create(tag).subscribe((obj) => {
-          this.tags.push(obj)
-        });
-      }});
-  }
+  constructor() {}
+  ngOnInit() {}
 }
