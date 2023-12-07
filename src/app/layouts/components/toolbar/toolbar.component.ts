03d49578d52febaa581c625940514aa5bf7a1b49
Lucas Nobrega <lucaspdn04@gmail.com>
2023-12-06 23:04:04 -0300

    make toolbar search independent

--- src/app/layouts/components/toolbar/toolbar.component.ts
+++ src/app/layouts/components/toolbar/toolbar.component.ts
@@ -5,26 +5,30 @@ import {map, Observable, startWith} from "rxjs";
 import {Router} from "@angular/router";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {SnackbarComponent} from "../snackbar/snackbar.component";
+import {MemoryboxFirestoreService} from "../../../shared/services/memorybox-firestore.service";
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
-  @Input() memoryBoxes: Array<MemoryBox> = new Array<MemoryBox>();
+  public memoryBoxes!: Array<MemoryBox>;
   public filteredMemoryBoxes: Observable<MemoryBox[]> = new Observable<MemoryBox[]>();
   public pesquisaControl = new FormControl('');
   public firstoption: string = "Memory Boxes";
   public CSSClasses: string | string[] = 'search-autocomplete';
 
-  constructor(private router: Router, private _snackbar: MatSnackBar, private elementRef: ElementRef) {
+  constructor(private memoryBoxService: MemoryboxFirestoreService, private router: Router, private _snackbar: MatSnackBar, private elementRef: ElementRef) {
   }
 
   ngOnInit() {
-    this.filteredMemoryBoxes = this.pesquisaControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
+    this.memoryBoxService.getAll().subscribe((arrayMemoryBoxes: MemoryBox[]) => {
+      this.memoryBoxes = arrayMemoryBoxes;
+      this.filteredMemoryBoxes = this.pesquisaControl.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filter(value || '')),
+      );
+    })
   }
 
   onOpened() {
